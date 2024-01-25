42099c1db199c1cb97b60c315f55a274e9b0cec5
sowmik-sec <sowmik.sec@gmail.com>
2024-01-25 12:04:07 +0600

    create frontend app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [jokes, setJokes] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`/api/jokes`)
+      .then((res) => setJokes(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <>
+      <h1>Hello world</h1>
+      <p>JOKES: {jokes.length}</p>
+      {jokes.map((joke) => (
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default App;
